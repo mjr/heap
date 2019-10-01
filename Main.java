30fc4a3329e01b7440f7d36c21828ebc96fdf2aa
Manaia Junior <manaiajr.23@gmail.com>
2019-09-30 23:36:28 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,30 @@
+package br.com.waldson.aula11;
+
+import br.com.waldson.aula12.FilaBanco;
+import br.com.waldson.aula12.Pessoa;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Heap / Priority Queue
+        Pessoa fulano  = new Pessoa("Fulano", 10);
+
+        FilaBanco fila = new FilaBanco();
+        fulano.addObserver(fila);
+//        fila.addPessoa("Fulano", 20);
+        fila.addPessoa("Sicrano", 10);
+        fila.addPessoa("Vovó", 64);
+        fila.addPessoa("Tio", 50);
+        fila.addPessoa(new Pessoa("Beltrano", 5));
+        fila.addPessoa(fulano);
+
+        fulano.setIdade(65);
+
+//        while (fila.getSize() > 0) {
+//            Pessoa p = fila.peek();
+//            System.out.println(p.getNome() + " está sendo atendido.");
+//            fila.remove();
+//        }
+
+    }
+}
